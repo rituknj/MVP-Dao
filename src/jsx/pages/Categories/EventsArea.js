9527f5530ad8086ab27b8306ada6db320c7edf34
mayank6253 <89696473+mynkbhati22@users.noreply.github.com>
2022-08-11 00:15:33 +0530

    betting-app-updated

--- src/jsx/pages/Categories/EventsArea.js
+++ src/jsx/pages/Categories/EventsArea.js
@@ -8,28 +8,28 @@ export default function EventsArea() {
     <div>
       <div className="container">
         <div className="row">
-          <div className="col-lg-9">
+          <div className="col-lg-9 col-md-9">
             <div className="event-area">
               <div className="event-content">
                 <Tabs
-                  defaultActiveKey="profile"
+                  defaultActiveKey="sports"
                   id="uncontrolled-tab-example"
                   className="mb-3"
                 >
-                  <Tab eventKey="home" title="SPORTS">
+                  <Tab eventKey="sports" title="SPORTS">
                     <SportsDetails/>
                   </Tab>
-                  <Tab eventKey="profile" title="E-SPORTS">
+                  <Tab eventKey="e-sports" title="E-SPORTS">
                     E-SPORTS
                   </Tab>
-                  <Tab eventKey="contact" title="EVERYTHING ELSE">
+                  <Tab eventKey="everything else" title="EVERYTHING ELSE">
                     EVERYTHING ELSE
                   </Tab>
                 </Tabs>
               </div>
             </div>
           </div>
-          <div className="col-lg-3"></div>
+          <div className="col-lg-3 col-md-3"></div>
         </div>
       </div>
     </div>
--- src/jsx/pages/Categories/SportsDetails.js
+++ src/jsx/pages/Categories/SportsDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import vector from "../../../images/Vector.png";
+import Group from "../../../images/Group 57.png";
 
 export default function SportsDetails() {
   return (
@@ -12,16 +13,26 @@ export default function SportsDetails() {
           </div>
           <div className="card betting-card">
             <div className="row">
-              <div className="col-md-6">
+              <div className="col-lg-2">
                 <h5 className="card-header">TITLE</h5>
                 <div className="card-body">
                   <p className="card-title">TEAM A</p>
                   <p className="card-title mt-4">TEAM A</p>
                 </div>
               </div>
-              <div className="col-md-6">
+              <div className="col-lg-7">
                 <div className="matched-event">
-                  <h6>MATCHED</h6>
+                  <img src={Group} alt="" className="match-img" />
+                </div>
+              </div>
+              <div className="col-lg-2">
+                <div className="pool-size">
+                  <span className="size">POOL SIZE</span>
+                </div>
+                <div className="amount">
+                  <p>
+                    <b>$3,600</b>
+                  </p>
                 </div>
               </div>
             </div>
