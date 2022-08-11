e6b3642bf0b41caee7684261c9dec5f931886292
kesaviwebsolutions <89696473+mynkbhati22@users.noreply.github.com>
2022-08-11 14:50:41 +0530

    updated-betting-app

--- src/jsx/pages/Categories/SportsDetails.js
+++ src/jsx/pages/Categories/SportsDetails.js
@@ -1,6 +1,8 @@
 import React from "react";
 import vector from "../../../images/Vector.png";
 import Group from "../../../images/Group 57.png";
+import Vector from "../../../images/Vector (2).png";
+import timer from "../../../images/carbon_timer.png";
 
 export default function SportsDetails() {
   return (
@@ -20,12 +22,45 @@ export default function SportsDetails() {
                   <p className="card-title mt-4">TEAM A</p>
                 </div>
               </div>
-              <div className="col-lg-7">
-                <div className="matched-event">
-                  <img src={Group} alt="" className="match-img" />
+              <div className="col-lg-10">
+                <div className="matched">
+                  <div className="matched-event">
+                    <img src={Group} alt="" className="match-img" />
+                  </div>
+                  <div className="pool-size">
+                    <span className="size">POOL SIZE</span>
+                    <div className="amount">
+                      <p>
+                        <b>$3,600</b>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="other-area">
+                  <div className="other-content">
+                    <img src={Vector} alt="" className="image-content" />
+                    <img src={vector} alt="" className="image-content" />
+                    <div className="time-day-content">
+                      <span className="time">14:00</span>
+                      <span className="day">SPET 31 </span>
+                    </div>
+                    <img src={timer} alt="" className="image-content" />
+                  </div>
+                  <div className="remaing-other-content">
+                    <p className="teams">
+                      Team A <br />
+                      <span className="percent">60%</span>
+                    </p>
+                    <p className="teams mx-3">
+                      DRAW <br /> <span className="percent">60%</span>
+                    </p>
+                    <p className="teams mx-3">
+                      Team A <br /> <span className="percent">60%</span>
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className="col-lg-2">
+              {/* <div className="col-lg-2">
                 <div className="pool-size">
                   <span className="size">POOL SIZE</span>
                 </div>
@@ -34,7 +69,7 @@ export default function SportsDetails() {
                     <b>$3,600</b>
                   </p>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
