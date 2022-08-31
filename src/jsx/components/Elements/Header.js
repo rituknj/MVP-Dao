1058bd4e51a87c52a8d709f8ed90490f3bed75c4
kesaviwebsolutions <89696473+mynkbhati22@users.noreply.github.com>
2022-08-31 10:50:53 +0530

    updated

--- src/jsx/components/Elements/Header.js
+++ src/jsx/components/Elements/Header.js
@@ -1,115 +1,131 @@
-import React, {useEffect, useStatem, Fragment, useState } from "react";
-import logo from '../../../images/logo.png';
+import React, { useEffect, useStatem, Fragment, useState } from "react";
+import logo from "../../../images/logo.png";
 import { Link, NavLink } from "react-router-dom";
-import {initInstance,getAccount, loginProcess} from './../../../web3/web3'
+import { initInstance, getAccount, loginProcess } from "./../../../web3/web3";
 
+const Header = () => {
+  const [account, setAccount] = useState();
+  const [modalShow, setModalShow] = useState(false);
+  const [allevents, setAllevents] = useState(0);
+  const [downloaded, setDownloaded] = useState(0);
 
+  useEffect(() => {
+    walletConnect();
+    setInterval(async () => {
+      try {
+        await getAddress();
+      } catch (error) {}
+    }, 3000);
+  }, []);
 
-const Header=()=> {
-    const [account, setAccount] = useState()
-    const [modalShow, setModalShow] = useState(false)
-    const [allevents, setAllevents] = useState(0)
-    const [downloaded, setDownloaded] = useState(0)
+  const walletConnect = async () => {
+    await loginProcess();
+    await initInstance();
+    const address = await getAccount();
+    window.login = address;
+    setAccount(address);
+  };
 
-    useEffect(()=>{
-        walletConnect();
-        setInterval(async()=>{
-           try {
-            await getAddress()
-           } catch (error) {
-            
-           }
-        },3000)
-    },[])
+  const getAddress = async () => {
+    const address = await getAccount();
+    window.login = address;
+    setAccount(address);
+  };
 
-   const walletConnect = async()=> {
-        await loginProcess();
-        await initInstance();
-        const address = await getAccount();
-        window.login = address
-        setAccount(address)
-    }
+  const slicing = (address) => {
+    const first = address.slice(0, 4);
+    const second = address.slice(38);
+    return first + "..." + second;
+  };
 
-    const getAddress =async()=>{
-        const address = await getAccount();
-        window.login = address
-        setAccount(address)
-    }
-
-    const slicing = (address)=> {
-        const first = address.slice(0,4);
-        const second = address.slice(38);
-        return first + "..." + second
-    }
-
-    // setInterval(()=>{
-    //     setDownloaded(Number(window.allEventstorde))
-    //     setAllevents(Number(window.allEvents))
-    // },2000)
-        return (
-            <Fragment>
-                <nav
-                    className="navbar navbar-expand-lg navbar-dark bg-dark bg-transparent"
-                    aria-label="Fifth navbar example"
-                >
-                    <div className="container-fluid home-header">
-                        <NavLink class="navbar-brand px-2 px-md-4" to="/">
-                            <img src={logo} width="50" className="py-1" />
-                        </NavLink>
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarsExample05"
-                            aria-controls="navbarsExample05"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarsExample05">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-                            <form>
-                                <ul className="navbar-nav">
-                                    {/* <li className="nav-item px-2 px-md-4">
+  // setInterval(()=>{
+  //     setDownloaded(Number(window.allEventstorde))
+  //     setAllevents(Number(window.allEvents))
+  // },2000)
+  return (
+    <Fragment>
+      <nav
+        className="navbar navbar-expand-lg navbar-dark bg-dark bg-transparent"
+        aria-label="Fifth navbar example"
+      >
+        <div className="container-fluid home-header">
+          <NavLink class="navbar-brand px-2 px-md-4" to="/">
+            <img src={logo} width="50" className="py-1" />
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarsExample05"
+            aria-controls="navbarsExample05"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarsExample05">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+            <form>
+              <ul className="navbar-nav">
+                {/* <li className="nav-item px-2 px-md-4">
                                         <a className="nav-link text-white mt-1" href="https://dao.betswamp.com" target='_blank'> TEST DAO</a>
                                     </li> */}
-                                    {/* {allevents > 0 && allevents == downloaded ? <li className="nav-item px-2 px-md-4">
+                {/* {allevents > 0 && allevents == downloaded ? <li className="nav-item px-2 px-md-4">
                                         <NavLink className="nav-link text-white mt-1" to="/app">MARKET</NavLink>
                                     </li>:
                                     <li className="nav-item px-2 px-md-4">
                                         <div className="nav-link text-white mt-1" to="/app"><Watch width='30' height='30' color='red'/></div>
                                     </li>} */}
-                                    <li className="nav-item px-2 px-md-4">
-                                        <a className="nav-link text-white mt-1" href="https://bet-swamp.gitbook.io/betswamp-v.2.0/" target='_blank'> DOCS</a>
-                                    </li>
-                                    {/* <li className="nav-item px-2 px-md-4">
+                <li className="nav-item px-2 px-md-4">
+                  <a
+                    className="nav-link text-white mt-1"
+                    href="https://bet-swamp.gitbook.io/betswamp-v.2.0/"
+                    target="_blank"
+                  >
+                    {" "}
+                    DOCS
+                  </a>
+                </li>
+                {/* <li className="nav-item px-2 px-md-4">
                                         <Link className="nav-link text-white mt-1" to="/betting-app" target="_blank">Betting App</Link>
                                     </li> */}
-                                    <li className="nav-item px-2 px-md-4">
-                                        <Link className="nav-link text-white mt-1" to="/tickets" target="_blank">Tickets</Link>
-                                    </li>
-                                    <li className="nav-item px-2 px-md-4">
-                                        <Link className="nav-link text-white mt-1" to="/betting-app" target="_blank">Betting App</Link>
-                                    </li>
-                                    <li className="nav-item px-2 px-md-4">
-                                        <Link className="nav-link text-white mt-1" to="/Validate" target="_blank">Validate</Link>
-                                    </li>
-                                    <li className="nav-item px-2 px-md-4">
-                                        <Link className="nav-link text-white mt-1" to="/Wallet" target="_blank">Wallet</Link>
-                                    </li>
-                                    <li className="nav-item px-2 px-md-4 navWallet" style={{cursor: "pointer"}}>
-                                        <span  className="nav-link text-white cursor-pointer" 
-                                        onClick={()=>walletConnect()}
-                                        >{account ? slicing(account) : "CONNECT"}</span>
-                                    </li>
-                                </ul>
-                            </form>
-                        </div>
-                    </div>
-                </nav>
-            </Fragment>
-        );
-    
-}
+                <li className="nav-item px-2 px-md-4">
+                  <Link className="nav-link text-white mt-1" to="/tickets">
+                    Tickets
+                  </Link>
+                </li>
+                <li className="nav-item px-2 px-md-4">
+                  <Link className="nav-link text-white mt-1" to="/betting-app">
+                    Betting App
+                  </Link>
+                </li>
+                <li className="nav-item px-2 px-md-4">
+                  <Link className="nav-link text-white mt-1" to="/Validate">
+                    Validate
+                  </Link>
+                </li>
+                <li className="nav-item px-2 px-md-4">
+                  <Link className="nav-link text-white mt-1" to="/Wallet">
+                    Wallet
+                  </Link>
+                </li>
+                <li
+                  className="nav-item px-2 px-md-4 navWallet"
+                  style={{ cursor: "pointer" }}
+                >
+                  <span
+                    className="nav-link text-white cursor-pointer"
+                    onClick={() => walletConnect()}
+                  >
+                    {account ? slicing(account) : "CONNECT"}
+                  </span>
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </Fragment>
+  );
+};
 export default Header;
--- src/jsx/pages/Categories/BettingFooter.js
+++ src/jsx/pages/Categories/BettingFooter.js
@@ -21,7 +21,7 @@ export default function BettingFooter() {
         <img src={footerbg} alt="" className="img-footer" />
 
         <div className="row">
-          <div className="col-lg-4 col-md-4 col-4" >
+          <div className="col-lg-4 col-md-4 col-4">
             <div className="footer-area">
               <div className="footer-content">
                 <div className="footer-img">
@@ -30,19 +30,22 @@ export default function BettingFooter() {
               </div>
             </div>
           </div>
-          <div className="col-4"  style={{marginTop:"3.5rem"}}>  <a href="/" target="_blank" className="mx-2">
-                    {" "}
-                    <img src={image} alt="" className="audit-link" />
-                  </a></div>
-          <div className="col-4" style={{marginTop:"3.5rem"}}>
-          <a href="/" target="_blank" className="mx-2">
-                    {" "}
-                    <img src={image2} alt="" className="audit-link" />
-                    <h4 className="audit-kyc">AUDIT AND KYC BY</h4>
-                  </a>
+          <div className="col-4" style={{ marginTop: "3.5rem" }}>
+            {" "}
+            <a href="/" target="_blank" className="mx-2">
+              {" "}
+              <img src={image} alt="" className="audit-link" />
+            </a>
           </div>
+          <div className="col-4" style={{ marginTop: "3.5rem" }}>
+            <a href="/" target="_blank" className="mx-2 text-decoration-none">
+              {" "}
+              <img src={image2} alt="" className="audit-link" />
+              <h4 className="audit-kyc">AUDIT AND KYC BY</h4>
+            </a>
           </div>
-          <div className="row ">
+        </div>
+        <div className="row ">
           <div className="col-lg-6 col-md-12">
             <div className="footer-area">
               <div className="footer-content">
@@ -64,82 +67,90 @@ export default function BettingFooter() {
                     <img src={eighteen} alt="" className="social-link" />
                   </a>
                 </div>
-                <div className="footer-audit-link">
-               
-                
-                
-</div>
+                <div className="footer-audit-link"></div>
 
+                {/* ............................................................................................................................................ */}
 
-{/* ............................................................................................................................................ */}
-
-
-            <div className="footer-area  footer-mobile" >
-              <div className="footer-content">
-                <div className="footer-social-link">
-
-<div className="row" style={{marginBottom:"2rem"}}>
-                <div className="col-lg-3 col-md-3 col-sm-6 col-6">
-                  <a href="/" target="_blank" className="mx-2">
-                    {" "}
-                    <img src={cryptokid} alt="" className="social-link"  />
-                  </a>
+                <div className="footer-area  footer-mobile">
+                  <div className="footer-content">
+                    <div className="footer-social-link">
+                      <div className="row" style={{ marginBottom: "2rem" }}>
+                        <div className="col-lg-3 col-md-3 col-sm-6 col-6">
+                          <a href="/" target="_blank" className="mx-2">
+                            {" "}
+                            <img
+                              src={cryptokid}
+                              alt=""
+                              className="social-link"
+                            />
+                          </a>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-6 col-6">
+                          <a href="/" target="_blank" className="mx-2">
+                            {" "}
+                            <img
+                              src={metfx}
+                              alt=""
+                              className="social-link social-link-2"
+                            />
+                          </a>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-6 col-6">
+                          <a href="/" target="_blank" className="mx-2">
+                            {" "}
+                            <img src={bet} alt="" className="social-link" />
+                          </a>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-6 col-6">
+                          <a href="/" target="_blank" className="mx-2">
+                            {" "}
+                            <img
+                              src={eighteen}
+                              alt=""
+                              className="social-link social-link-2"
+                            />
+                          </a>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className="col-lg-3 col-md-3 col-sm-6 col-6">
-                  <a href="/" target="_blank" className="mx-2">
-                    {" "}
-                    <img src={metfx} alt="" className="social-link social-link-2"  />
-                  </a></div>
-                  <div className="col-lg-3 col-md-3 col-sm-6 col-6">
-                  <a href="/" target="_blank" className="mx-2">
-                    {" "}
-                    <img src={bet} alt="" className="social-link"  />
-                  </a></div>
-                  <div className="col-lg-3 col-md-3 col-sm-6 col-6">
-                  <a href="/" target="_blank" className="mx-2">
-                    {" "}
-                    <img src={eighteen} alt="" className="social-link social-link-2" />
-                  </a></div>
-                  </div>
-
-</div></div></div></div></div>
-
-
-
-
-
                 </div>
-           
-           
-          
+              </div>
+            </div>
           </div>
-       
+        </div>
+
         <BettingFooterItem />
         <div className="row">
-          <div className="col-lg-12 col-md-6 col-sm-6 " >
+          <div className="col-lg-12 col-md-6 col-sm-6 ">
             <div className="footer-social-link-items text-center">
               <div className="social mb-3">
-              <div className="row">
-              <div className="col-2">
-                <a href="/" target="_blank" className="mx-3">
-                  <img src={fb} alt="" />
-                </a></div>
-                <div className="col-2">
-                <a href="/" target="_blank" className="mx-3">
-                  <img src={tweet} alt="" />
-                </a></div>
-                <div className="col-2">
-                <a href="/" target="_blank" className="mx-3">
-                  <img src={insta} alt="" />
-                </a></div>
-                <div className="col-2">
-                <a href="/" target="_blank" className="mx-3">
-                  <img src={sl} alt="" />
-                </a></div>
-                <div className="col-2">
-                <a href="/" target="_blank" className="mx-3">
-                  <img src={telegram} alt="" />
-                </a></div>
+                <div className="row">
+                  <div className="col-2">
+                    <a href="/" target="_blank" className="mx-3">
+                      <img src={fb} alt="" />
+                    </a>
+                  </div>
+                  <div className="col-2">
+                    <a href="/" target="_blank" className="mx-3">
+                      <img src={tweet} alt="" />
+                    </a>
+                  </div>
+                  <div className="col-2">
+                    <a href="/" target="_blank" className="mx-3">
+                      <img src={insta} alt="" />
+                    </a>
+                  </div>
+                  <div className="col-2">
+                    <a href="/" target="_blank" className="mx-3">
+                      <img src={sl} alt="" />
+                    </a>
+                  </div>
+                  <div className="col-2">
+                    <a href="/" target="_blank" className="mx-3">
+                      <img src={telegram} alt="" />
+                    </a>
+                  </div>
                 </div>
               </div>
               <span className="copy-right">© 2021 Betswamp.com</span>
